695cdfbf2b5c2416b65d341896cbac306d69caf3
cbaspree <sebastian.bravot@gmail.com>
2024-05-09 17:04:59 -0400

    feature: Add constructor to create matrix from 2d array representation

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[][] matrix1 =
+            [
+                [5, 0, 0, 0],
+                [0, 8, 0, 0],
+                [0, 0, 3, 0],
+                [0, 6, 0, 0],
+            ];
+
+            int[][] matrix2 =
+            [
+                [10, 20, 0, 0, 0, 0],
+                [0, 30, 0, 40, 0, 0],
+                [0, 0, 50, 60, 70, 0],
+                [0, 0, 0, 0, 0, 80],
+            ];
+
+
+            SparseMatrix sparseMatrix1 = new SparseMatrix(matrix1);
+            sparseMatrix1.Print();
+
+            SparseMatrix sparseMatrix2 = new SparseMatrix(matrix2);
+            sparseMatrix2.Print();
         }
     }
 }
